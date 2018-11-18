787a6c3597dfb1f69b3e178c475fbf1eee9eb5d1
andreasv99 <t8170015@dias.aueb.gr>
2018-11-18 11:19:55 +0200

    Add maven tree in my branch

---
+++ DatabaseByDatabasics/src/main/java/jar/Attribute.java
@@ -0,0 +1,44 @@
+package jar;
+
+import java.util.ArrayList;
+
+//This class is still so simple that documents itself really.
+
+public class Attribute {
+	private String name;
+	private String type;
+	private ArrayList<String> array = new ArrayList<String>();
+
+	public Attribute(String name, String type) {
+		this.name = name;
+		this.type = type;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	//The user shouldn't have the ability to change a column's type before deleting the data
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public ArrayList<String> getArray() {
+		return array;
+	}
+
+	public void setEntryField(String entry) {
+		array.add(entry);
+	}
+
+}
+
+
