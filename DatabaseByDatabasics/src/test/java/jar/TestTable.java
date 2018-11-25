d0c8599ad2c03ff48e5bc8a8f2a1655563a64af4
Evi Vratsanou <evratsanou@gmail.com>
2018-11-25 14:17:44 +0200

    Starting Tests

---
+++ DatabaseByDatabasics/src/test/java/jar/TestTable.java
@@ -0,0 +1,12 @@
+package jar;
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.After;
+public class TestTable extends TestCase {
+	private Table table = new Table("Student");
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+}
