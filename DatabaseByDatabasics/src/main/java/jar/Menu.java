3941843b905d0f64eab4f42bd1544a374a4f5e1a
mpontika <44073622+mpontika@users.noreply.github.com>
2018-12-02 20:27:49 +0200

    Menu updated with delete Menus

--- DatabaseByDatabasics/src/main/java/jar/Menu.java
+++ DatabaseByDatabasics/src/main/java/jar/Menu.java
@@ -283,4 +283,124 @@ public class Menu {
 		Scanner input = new Scanner(System.in);
 		return input.nextInt();
 	}
+	
+	public static void menuDelete() {
+		Scanner input = new Scanner(System.in);
+		boolean flag = true;
+		boolean correctChoice;
+		while (flag) {
+			int choice = -1;
+			do {
+				correctChoice = true;
+				printDeleteChoices();
+				try {
+					choice = input.nextInt();
+					if(choice != 1 && choice != 2 && choice != 3 && choice != 4) {
+						correctChoice = false;
+						System.out.println("This was not one of the menu's choices");
+					}
+				} catch (InputMismatchException err) {
+					System.out.println("This was not a number!");
+					correctChoice= false;
+					input.next();
+					continue;
+				}
+			}	while (correctChoice == false);
+			flag = printDeleteMessages(choice,flag);
+		}
+	}
+
+	public static boolean printDeleteMessages(int choice, boolean flag) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Are you sure you want to delete this?");
+		System.out.println("Press 'yes' to continue or 'no' to exit Delete Mode");
+		String d = input.nextLine();
+		if (!(d.equalsIgnoreCase("yes"))) {
+			delete(choice);
+			System.out.println("Do you want to delete anything else?");
+			System.out.println("Press 'yes' to continue or 'no' to exit Copy Mode");
+			String ch = input.nextLine();
+			if (!(ch.equalsIgnoreCase("yes"))) {
+				flag = true;
+			} else {
+				flag = false;
+			}
+		} else {
+			flag = false;
+		}
+		return flag;
+	}
+
+	public static void printDeleteChoices() {
+		System.out.println("What do you want to delete;");
+		System.out.println("	1.A table");
+		System.out.println("	2.An entry");
+		System.out.println("	3.A whole attribute");
+		System.out.println("	4.Just an element");
+		System.out.println("Please enter the number of your choice");
+	}
+
+
+
+	public static void menuDeleteTable() {
+		String tableName;
+		System.out.println("Please enter the name of the table that you want to delete");
+		tableName = readTable();
+		Table.getTables(Table.position(tableName)).deleteTable(tableName);
+	}
+
+	public static int readLines(String tableName) {
+		Scanner input = new Scanner(System.in);
+		int lineNum = input.nextInt();
+		while (lineNum > Table.getTables(Table.position(tableName)).getLines()) {
+			System.out.println("This line does not exist. Please type an existing line.");
+			lineNum = input.nextInt();
+		}
+		return lineNum;
+	}
+
+	public static void menuDeleteEntry() {
+		String tableName;
+		System.out.println("Please enter the name of the table that contains the entry you want to delete");
+		tableName = readTable();
+		int line;
+		System.out.println("Please enter the number of the entry that you want to delete");
+		line = readLines(tableName);
+		Table.getTables(Table.position(tableName)).deleteEntry(tableName,line-1);
+	}
+
+	public static void menuDeleteAttribute() {
+		String tableName;
+		System.out.println("Please enter the name of the table that contains the attribute you want to delete");
+		tableName = readTableName();
+		System.out.println("Please enter the name of the attribute that you want to delete");
+		String attName = readAttribute(tableName);
+		Table.getTables(Table.position(tableName)).deleteAttribute(tableName,attName);
+	}
+
+	public static void menuDeleteElement() {
+		String tableName;
+		System.out.println("Please enter the name of the table that contains the element that you want to delete");
+		tableName = readTable();
+		System.out.println("Please enter the name of the attribute that contains the element that you want to delete");
+		String attName = readAttribute(tableName);
+		System.out.println("Please enter the number of the element line that you want to delete");
+		int line = readLines(tableName);
+		Table.getTables(Table.position(tableName)).deleteElement(tableName, line-1, attName);
+	}
+
+
+	public static void delete(int choice) {
+		switch (choice) {
+			case 1:
+				menuDeleteTable();
+			case 2:
+				menuDeleteEntry();
+			case 3:
+				menuDeleteAttribute();
+			case 4:
+				menuDeleteElement();
+			}
+
+	}
 }
