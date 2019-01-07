8242b76e86800e7af999e9e3c08f5b9eb10a7574
Evi Vratsanou <evratsanou@gmail.com>
2019-01-08 01:08:09 +0200

    Adding areYouSure method where is necessary

--- DatabaseByDatabasics/src/main/java/gr/aueb/databasics/CommandLineMenu.java
+++ DatabaseByDatabasics/src/main/java/gr/aueb/databasics/CommandLineMenu.java
@@ -81,6 +81,10 @@ public class CommandLineMenu {
 				databaseMenu();
 			}
 			if (choice == 3) {
+				boolean sure = areYouSure();
+				if (!sure) {
+					databaseChoiceMenu();
+				}
 				System.out.println("Type in the name of the "
 						+ "database you want to delete.");
 				String s = readDatabase();
@@ -92,8 +96,12 @@ public class CommandLineMenu {
 			if (choice == 4) {
 				DatabaseUniverse.listDatabases();
 			}
-			if (choice == 5) {	
-					programTermination();
+			if (choice == 5) {
+				boolean sure = areYouSure();
+				if (!sure) {
+					printDeleteChoices();
+				}
+				programTermination();
 			}
 		}
 	}
@@ -560,12 +568,20 @@ public class CommandLineMenu {
 				return;
 			}
 			if (choice == 1) {
+				boolean sure = areYouSure();
+				if (!sure) {
+					databaseMenu();
+				}
 				addTable();
 				activeDatabase.setSaved(false);
 				viewDatabase();
 			} else if (choice == 2) {
 				viewTableMenu();
 			} else if (choice == 3) {
+				boolean sure = areYouSure();
+				if (!sure) {
+					databaseMenu();
+				}
 				deleteTableMenu();
 				activeDatabase.setSaved(false);
 				viewDatabase();
@@ -645,6 +661,10 @@ public class CommandLineMenu {
 		} else if (choice == 3) {
 			viewOptions();
 		} else if (choice == 4) {
+			boolean sure = areYouSure();
+			if (!sure) {
+				tableMenu();
+			}
 			addAttributeOptions();
 			activeDatabase.setSaved(false);
 		} else if (choice == 5) {
@@ -654,6 +674,10 @@ public class CommandLineMenu {
 			changeDataOptions();
 			activeDatabase.setSaved(false);
 		} else if (choice == 7) {
+			boolean sure = areYouSure();
+			if (!sure) {
+				tableMenu();
+			}
 			deleteMenu();
 			activeDatabase.setSaved(false);
 		} else if (choice == 8) {
@@ -1323,6 +1347,10 @@ public class CommandLineMenu {
 	public static void deleteMenu() {
 		printDeleteChoices();
 		int choice = checkChoice(1, 3);
+		boolean sure = areYouSure();
+		if (!sure) {
+			deleteMenu();
+		}
 		if (choice == -1) {
 			return;
 		}
